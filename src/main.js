f82d9ecdf7f9a84aec1b414dd77e920b2453395f
shenhl <291233404@qq.com>
2018-06-01 14:31:40 +0800

    my push

---
+++ src/main.js
@@ -0,0 +1,18 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+/* eslint-disable */
+import Vue from 'vue';
+import App from './App';
+import router from './router';
+import VueRouter from 'vue-router';
+
+Vue.config.productionTip = false;
+
+const vRouter = new VueRouter();
+
+/* eslint-disable no-new */
+new Vue({
+  router,
+  components: { App },
+  template: '<App/>'
+}).$mount('#app');
